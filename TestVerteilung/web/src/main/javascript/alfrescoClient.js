5a3ef68b1b4939e98f5fc93a3ff3540bd7378849
klaus <klaus.schulte65@gmail.com>
2015-01-28 00:00:38 +0100

    Suchen

--- TestVerteilung/web/src/main/javascript/alfrescoClient.js
+++ TestVerteilung/web/src/main/javascript/alfrescoClient.js
@@ -200,22 +200,24 @@ function loadLayout() {
             initPanes: true,
             resizeWithWindow: false,
             contentSelector: ".ui-widget-content",
-            north: {
-                paneSelector: "#searchNorth",
-                name: "searchNorthLayout",
-                size: 85,
-                fxSettings_open: {easing: "easeOutBounce"},
-                closable: false,
-                resizable: false,
-                slidable: false
-            },
-            center: {
-                paneSelector: "#searchCenter",
-                name: "searchCenterLayout",
-                minHeight: 80,
-                size: .8,
-                resizable: true,
-                slidable: true
+            children: {
+                north: {
+                    paneSelector: "#searchNorth",
+                    name: "searchNorthLayout",
+                    size: 85,
+                    fxSettings_open: {easing: "easeOutBounce"},
+                    closable: false,
+                    resizable: false,
+                    slidable: false
+                },
+                center: {
+                    paneSelector: "#searchCenter",
+                    name: "searchCenterLayout",
+                    minHeight: 80,
+                    size: .8,
+                    resizable: true,
+                    slidable: true
+                }
             }
         };
         var alfrescoLayoutSettings = {
@@ -1037,7 +1039,7 @@ function loadAlfrescoSearchTable() {
 
         $("#alfrescoSearchTabelle_info").detach().appendTo('#alfrescoSearchTableFooter');
         $("#alfrescoSearchTabelle_paginate").detach().appendTo('#alfrescoSearchTableFooter');
-
+        $('#alfrescoSearchTableFooter').width(searchLayout.center.state.innerWidth);
         // Add event listener for opening and closing details
         $('#dtable4 tbody').on('click', 'td.details-control', function () {
             var tr = $(this).closest('tr');
@@ -1627,7 +1629,7 @@ function startSearch(searchText) {
         ], null, true);
         if (json.success) {
             alfrescoSearchTabelle.clear();
-            //$('#alfrescoSearchTableFooter').width(alfrescoLayout.center.children.alfrescoCenterInnerLayout.state.center.innerWidth);
+            $('#alfrescoSearchTableFooter').width(searchLayout.center.state.innerWidth);
             alfrescoSearchTabelle.rows.add(json.result).draw();
             calculateTableHeight("searchCenter", alfrescoSearchTabelle, "dtable4", "alfrescoSearchTabelle", "alfrescoSearchTabelleHeader", "alfrescoSearchTableFooter");
             $.fn.dataTable.makeEditable(alfrescoSearchTabelle, {
@@ -2236,6 +2238,11 @@ function start() {
         var countryList = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina Faso", "Burma", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros", "Congo, Democratic Republic", "Congo, Republic of the", "Costa Rica", "Cote d'Ivoire", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Fiji", "Finland", "France", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Greenland", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, North", "Korea, South", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova", "Mongolia", "Morocco", "Monaco", "Mozambique", "Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Norway", "Oman", "Pakistan", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Qatar", "Romania", "Russia", "Rwanda", "Samoa", "San Marino", " Sao Tome", "Saudi Arabia", "Senegal", "Serbia and Montenegro", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "Spain", "Sri Lanka", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Togo", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Yemen", "Zambia", "Zimbabwe"];
         $("#alfrescoSearch").autocomplete({
             source: countryList
+        });
+        $('#alfrescoSearch').on('keypress', function (event) {
+            if(event.which === 13){
+                startSearch($(this).val());
+            }
         });
         viewMenu = $('#menu-1').superfish();
         viewMenu = $('#menu-2').superfish();
